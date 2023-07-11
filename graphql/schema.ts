cb86513123279adbfe5b031df669f0bdf5357522
Frank Collins <fwc3rd@gmail.com>
2023-07-11 15:08:12 -0400

    schema so far [3:07pm]

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -16,13 +16,53 @@ export const typeDefs = gql`
         name: String
     }
 
-    type Query {
-        links: [Link]!     
-        pokemon: Pokemon
+    type Settings {
+        id: Int,
+        age: Int,
+        weight: Int,
+        height: Int,
+        reminder: Int,
+        activity: Int,
+        start_time: Int,
+        end_time: Int,
+        users_id: Int
     }
 
+    type Users {
+        id Int,
+        google_id: String,
+        icon: String,
+        username: String,
+        password: String,
+        email: String,
+        age: Int
+    }
+
+    type Data {
+        id: Int,
+        google_id: String,
+        date: String,
+        progress: String,
+        weekday: String,
+        status: [String],
+        users_id: Int
+    }
+
+    type ENV {
+        DATABASE_URL: String,
+        API: String,
+        NODE_ENV: String,
+        GOOGLE_ID: String,
+        WEATHER_KEY: String
+    }
+
+    type Query {
+        links: [Link]!     
+        pokemon: Pokemon,
+        settings: Settings,
+        users: Users,
+        data: Data,
+        env: ENV,        
+    }
     `
     //     type Query { }     // reads This is saying return the resolver "links" with array of [Link]
-    //      
-
-    // id, category, description, imageUrl, users, title, url
