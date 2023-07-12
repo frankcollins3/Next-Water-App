b03ea79d794502c82908305703af47fa2130e106
Frank Collins <fwc3rd@gmail.com>
2023-07-12 02:58:42 -0400

    fix issue #28 graphQL post route [2:58am]

--- components/webpage/Main/Main.tsx
+++ components/webpage/Main/Main.tsx
@@ -9,61 +9,79 @@ import Container from 'react-bootstrap/Container';
 // redux state management
 import { RootState } from 'redux/store/rootReducer';
 import { useSelector, useDispatch } from 'react-redux';
-import { toggleHydroSettings } from 'redux/main/mainSlice'
+import { TOGGLE_HYDRO_SETTINGS } from 'redux/main/mainSlice'
 
 // utility functions
+import WaterRequestRead from "utility/WaterRequestRead"
 // import testfunc from "pages/api/test"
+
+// graphql queries 
+import {allDBsettingsquery} from "graphql/queries"
 
 export default function Main(props: any) {
   useEffect(() => {
     // Your useEffect logic here
   }, []);
 
-  const hydroSettings = useSelector((state: RootState) => state.main.HYDRO_SETTINGS);
+  const HYDRO_SETTINGS = useSelector((state: RootState) => state.main.HYDRO_SETTINGS);
 
   
   return (
     <Container id={styles.Page_1}>
-      <RenderMain hydroSettings={hydroSettings} />
+      <RenderMain HYDRO_SETTINGS={HYDRO_SETTINGS} />
     </Container>
   );
 }
 
-function RenderMain({ hydroSettings }: { hydroSettings: boolean }) {
+function RenderMain({ HYDRO_SETTINGS }: { HYDRO_SETTINGS: boolean }) {
   const dispatch = useDispatch()
 
   const yestest = async () => {
 
-    const query = `
-  query {
-    hello
-  }
-`;
+    dispatch(TOGGLE_HYDRO_SETTINGS())
 
-const fetchData = async () => {
-  try {
-    const response = await axios.post('/api/graphql', {
-      query: `
-        query {
-          pokemon {
-            name
-          }
+//  const allDBsettingsquery = `query { allDBsettings { id, height, weight, age, start_time, end_time, reminder, users_id } }`
+   
+    // works
+axios
+  .post('/api/graphql', {
+    query: `
+      query {
+        addUserSettings(
+          id: 3,
+          age: 25,
+          height: 90,
+          weight: 190,
+          start_time: 2,
+          end_time: 10,
+          reminder: 1,
+          activity: 1,
+          users_id: 3
+        ) {
+          id
+          age
+          height
+          weight
+          start_time
+          end_time
+          reminder
+          activity
+          users_id
         }
-      `,
-    });
-    console.log(response.data);
-    // Process the returned data
-  } catch (error) {
-    console.error('GraphQL fetch error:', error);
+      }
+    `
+  })
+  .then((response) => {
+    console.log('Response:', response.data);
+    // Process the response data
+  })
+  .catch((error) => {
+    console.log('Error:', error);
     // Handle the error
-  }
-};
+  });
 
-
-fetchData()
     
-}
-
+} // end of yestest
 
   return (
     <>
@@ -77,9 +95,10 @@ fetchData()
       </Container>
 
       <Container className={styles.panel}>
-        {/* Use the hydroSettings state here */}
-        <p> { hydroSettings === true ? "yes" : "no" } </p>
         <button onClick={yestest}> test me! </button>
+        {/* Use the hydroSettings state here */}
+        { HYDRO_SETTINGS === true && <Dropinbucket></Dropinbucket> }
+        {/* <p> { HYDRO_SETTINGS === true ? "yes" : "no" } </p> */}
         {/* {hydroSettings && <p> panel </p>} */}
       </Container>
     </>
