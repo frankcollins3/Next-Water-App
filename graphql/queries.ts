438f5a2cfaa3b765761b78708a54fc6bf73b26a9
Frank Collins <fwc3rd@gmail.com>
2023-07-14 23:45:25 -0400

    queries that don't need param here [11:44pm]

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,1 +1,4 @@
 export const allDBsettingsquery = `query { allDBsettings { id, height, weight, age, start_time, end_time, reminder, users_id } }`
+export const allDBdataquery = `query { allDBdata { id, google_id, date, progress, weekday, status, users_id } }`
+export const allDBusersquery = `query { allDBusers { username, email, password, icon, google_id  } }`
+// id | google_id | date | progress | weekday | status | users_id 
