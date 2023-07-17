e270e01b97062739c4e40d1d8d0fa85782d74b4b
Frank Collins <fwc3rd@gmail.com>
2023-07-17 12:48:56 -0400

    surprise: redis persists through shutdown [12:48pm]

--- redis/redis.js
+++ redis/redis.js
@@ -1,4 +1,5 @@
 // redis.js createClient. This is so a /pages.file.tsx ----> multiple components can import the redisClient and use the cached data.
+// bookmarking crazy fact that if the computer is shut off and unplugged the cache persists since redis runs off of RAM. 
 import redis from 'redis';
 const redisClient = redis.createClient();
 export default redisClient;
