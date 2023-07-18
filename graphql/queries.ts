75f2c3b3814eb7759a7a439c023a7eab37c6cca6
Frank Collins <fwc3rd@gmail.com>
2023-07-18 10:10:12 -0400

    callback func that takes params to login [10:09am]

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -1,4 +1,22 @@
 export const allDBsettingsquery = `query { allDBsettings { id, height, weight, age, start_time, end_time, reminder, users_id } }`
 export const allDBdataquery = `query { allDBdata { id, google_id, date, progress, weekday, status, users_id } }`
-export const allDBusersquery = `query { allDBusers { username, email, password, icon, google_id  } }`
-// id | google_id | date | progress | weekday | status | users_id 
+export const allDBusersquery = `query { allDBusers { id, google_id, username, password, email, age, icon } }`
+
+export const userLoginQueryStringFunc = (email:string, password:string) => {
+    // args have to be within double quotes for strings!
+const query = `
+       query {
+         userLogin(email: "${email}", password: "${password}" ) {       
+           username,
+           password,
+           email,
+           age,
+           icon,
+           token          
+         }
+       }
+     `
+    return query;
+}
+
+// data: id | google_id | date | progress | weekday | status | users_id 
