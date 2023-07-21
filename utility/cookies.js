565c2c187cfaa2cb96b0716e7273edcf92e5c090
Frank Collins <fwc3rd@gmail.com>
2023-07-20 20:53:30 -0400

    get ID from token [8:52pm]
    almost posted issue with lastIndexOf(":") to get the:
    id: 8 id separated from 8 by the colon 
    
    token:
    token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI…Q.7UU2jTYt3NdFuGAHtpLMWLgm7tdtioMjxmOELBmSS7wid:8
    
    (2) ['token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI…zI1fQ.7UU2jTYt3NdFuGAHtpLMWLgm7tdtioMjxmOELBmSS7w', '8']
    
    ['8'] user id separated

--- utility/cookies.js
+++ utility/cookies.js
@@ -19,4 +19,14 @@ export function clearCookie(name) {
     document.cookie = `${name}=; expires=Thu, 11 Nov 1864 00:00:00 UTC; path=/;`;
   }
 
+export function userIdFromCookie(name) {
+    name = name[0]
+    const splitNameForId = name.split("id:")
+    console.log(splitNameForId)
+
+    let userId = splitNameForId[1]
+    return userId
+    // let preregexID = name[0].substring(preIdIndex + 1);
+}
+
 // document.cookie = `token=${userLogin.token}; max-age=${7 * 24 * 60 * 60}; path=/;`;
