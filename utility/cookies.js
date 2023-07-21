42285cd2047e51cc79b0e59136274e6928b8362a
Frank Collins <fwc3rd@gmail.com>
2023-07-21 13:53:04 -0400

    modularize cookie promise [1:53pm]

--- utility/cookies.js
+++ utility/cookies.js
@@ -19,14 +19,36 @@ export function clearCookie(name) {
     document.cookie = `${name}=; expires=Thu, 11 Nov 1864 00:00:00 UTC; path=/;`;
   }
 
-export function userIdFromCookie(name) {
-    name = name[0]
-    const splitNameForId = name.split("id:")
-    console.log(splitNameForId)
+  export async function userIdFromCookie(name) {
+    if (name === null || name === undefined || name === '') return null
 
-    let userId = splitNameForId[1]
-    return userId
+    if (name[0] !== undefined || name[0] !== undefined || name[0] !== ' ') {
+      const splitChar = name[0].split('***')
+      const userId = splitChar[1]
+      return userId ? userId : null    
+      // const splitChar = name[0].split('***') || name.split('***')
+    } else {
+      console.log("hey were over here")
+    }
+    // const splitChar = name.split('***') || name[0].split('***')
+    
     // let preregexID = name[0].substring(preIdIndex + 1);
 }
+
+export const remembermecookiePROMISE = new Promise( (cookies, milk) => {
+  // get cookie and return which will include both the token and the id.
+  const precookie = getCookie()           
+  cookies(precookie)
+  milk("spill")
+})
+remembermecookiePROMISE
+.then(async(c) => {
+  let cookieIdString = c[1]
+  const sliceID = cookieIdString.slice(3)
+  return sliceID || "no id"
+  // let cookieID = cookieIdString.replace(RreturnNumbers, '') // replace doesn't exist on string or object
+})
+
+ 
 
 // document.cookie = `token=${userLogin.token}; max-age=${7 * 24 * 60 * 60}; path=/;`;
