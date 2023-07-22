2b73de74378b9809a526f06a29f51eccde2ba715
Frank Collins <fwc3rd@gmail.com>
2023-07-22 16:02:31 -0400

    settings interface -> graphQL addSettings mutation [4:02pm]

--- utility/interfaceNtypes.ts
+++ utility/interfaceNtypes.ts
@@ -1,2 +1,13 @@
 // type Pokemon = { name: String }
 export interface PokemonInterface { name: String }
+
+export interface UsersLoginInterface { id: number, google_id: string, icon: string, username: string, password: string, email: string, age: number, token: string }
+
+export interface SelectedIconImgStr { img: string }
+
+export interface MainInterface {
+    HYDRO_SETTINGS: boolean;
+}
+
+export interface SettingsInterface { id:number, age:number, height:number, weight:number, start_time:number, end_time:number, reminder:number, activity:number, users_id:number }
+
