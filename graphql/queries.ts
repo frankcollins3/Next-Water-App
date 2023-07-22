11e4f4c415752f30bff39ab50ebcafd2f59f3aaa
Frank Collins <fwc3rd@gmail.com>
2023-07-22 17:36:04 -0400

    new query string returning func for gQL (delete user settings) [5:35pm]

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -37,3 +37,48 @@ export const userSignupQueryStringFunc = (username, password, email, age, icon, 
   return query;
 }
 
+export const addUserSettingsQueryStringFunc = (age:number, height:number, weight:number, start_time:number, end_time:number, reminder:number, activity:number, users_id:number ) => {
+  const query = `
+  mutation {
+    addUserSettings(age: ${age}, height: ${height}, weight: ${weight}, start_time: ${start_time}, end_time: ${end_time}, reminder: ${reminder}, activity: ${activity}, users_id: ${users_id}) {
+      id
+      age,
+      height,
+      weight,
+      start_time,
+      end_time,
+      reminder,
+      activity,
+      users_id
+    }
+  }
+  `
+  return query;
+}
+
+export const deleteUserSettingsQueryString = (users_id:number) => {
+  const query = `
+  mutation { deleteUserSettings(users_id: ${users_id}) {
+      id,
+      age,
+      height,
+      weight,
+      start_time,
+      end_time,
+      reminder,
+      activity,
+      users_id
+    }
+  }
+  `
+  return query
+}
+
+      // age: 25,
+      // height: 90,
+      // weight: 190,
+      // start_time: 2,
+      // end_time: 10,
+      // reminder: 1,
+      // activity: 1,
+      // users_id: 3
