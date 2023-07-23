1ed1fdabb1be95b708273e68fbdf8a1c5cc6e9b5
Frank Collins <fwc3rd@gmail.com>
2023-07-22 22:31:13 -0400

    fix issue #84 and SET_REDUX() within Context/Promises.tsx [10:31pm]

--- Contexts/Promises.tsx
+++ Contexts/Promises.tsx
@@ -1,6 +1,15 @@
 import React, { createContext, useContext, ReactNode, useState } from "react";
 import { userSettingsQueryString } from "graphql/queries";
 import axios from "axios"
+import { SettingsInterface } from "utility/interfaceNtypes";
+import Settings from "components/elements/Settings";
+import Schedule from "components/elements/Schedule/Schedule";
+
+// @redux/toolkit global state management
+import {RootState} from "redux/store/rootReducer"
+import {useSelector, useDispatch} from "react-redux"
+
+import { SET_HYDRO_SCHEDULE } from "redux/main/mainSlice"
 
 
 type PromiseTypes = {
@@ -8,13 +17,15 @@ type PromiseTypes = {
 
     iPROMISEcookies: () => any;
     getUserSettingsPROMISE: () => any;
+    userSettingsSchedulePROMISE: () => any;
 }
 
 const PromiseDefaults = {
     tokenID: 1,
 
     iPROMISEcookies: () => {},
-    getUserSettingsPROMISE: () => {}
+    getUserSettingsPROMISE: () => {},
+    userSettingsSchedulePROMISE: () => {}
 }
 
 const PromiseContext = createContext<PromiseTypes>(PromiseDefaults)
@@ -26,6 +37,8 @@ export function usePromise() {
 type Props = { children: ReactNode }
 
 export function PromiseProvider({children}:Props) {
+    const dispatch = useDispatch()
+
     const [tokenID, setTokenID] = useState<number>(0)
 
     function iPROMISEcookies () {
@@ -43,26 +56,48 @@ export function PromiseProvider({children}:Props) {
         })
     }
 
+    // SETTINGS PROMISES! 
     function getUserSettingsPROMISE () {
         return iPROMISEcookies()
         .then(async(cookieID) => {
             const ID = parseInt(cookieID)
             const userSettingsQueryStr = await userSettingsQueryString(ID)
             let mySettings = await axios.post('/api/graphql', { query: `${userSettingsQueryStr}` } )
+
             return mySettings
         })        
-        // return (async() => {            
-
-        //     const userSettingsQueryStr = await userSettingsQueryString()
-        //     let mySettings = await axios.post('/api/graphql', { query: `${userSettingsQueryStr}` } )
-        //     return mySettings
-        // })()
     }
+
+    async function userSettingsSchedulePROMISE() {
+        let schedule:any[] = []
+        try {
+            const mySettings = (await getUserSettingsPROMISE()).data.data.userSettings;   
+            console.log(mySettings)         
+    if (!mySettings) {
+        return "no settings"
+    } else if (mySettings) {              
+        console.log('mySettings', mySettings)
+        const SchedulePromise = new Promise(async(resolve:any, reject:any) => {
+            const setSchedule = () => { for (let i = mySettings.start_time; i <= mySettings.end_time; i += mySettings.reminder) { schedule.push(i)} }
+            await setSchedule()
+        resolve(schedule.length ? schedule : "schedule fail")
+      })
+      return SchedulePromise
+      .then( (schedule) => {
+        dispatch(SET_HYDRO_SCHEDULE(schedule))
+        return schedule
+      })
+    }
+    }
+    catch { return "aaye catch" }
+}
+
 
         const value = {
             tokenID,
             iPROMISEcookies,
-            getUserSettingsPROMISE
+            getUserSettingsPROMISE,
+            userSettingsSchedulePROMISE
         }        
 
         // let cookieID = cookieIdString.replace(RreturnNumbers, '') // replace doesn't exist on string or object
