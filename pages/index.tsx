97e00123a6267499167f5e6ea01e6acf59696bb2
Frank Collins <fwc3rd@gmail.com>
2023-07-29 19:13:33 -0400

    fixed foreign key issue with routes  [7:13pm]
    set promises up to init app and current user

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,70 +5,80 @@ import axios from 'axios'
 import Dropinbucket from 'components/elements/DropInBucket';
 import Settings from 'components/elements/Settings';
 import Container from 'react-bootstrap/Container';
+import Schedule from "components/elements/Schedule"
+import Panel from "components/elements/Panel"
 import styles from 'components/webpage/Main/Main.module.scss'
+
 
 // @redux/toolkit state management
 import { RootState } from 'redux/store/rootReducer';
 import { useSelector, useDispatch } from 'react-redux';
-import { TOGGLE_HYDRO_SETTINGS, SET_CURRENT_PAGE, SET_HYDRO_INTAKE } from 'redux/main/mainSlice'
+import { TOGGLE_HYDRO_SETTINGS, SET_CURRENT_PAGE, SET_HYDRO_INTAKE, SET_HYDRO_SCHEDULE, SET_HYDRO_DATA, SET_STATUS, SET_DATE, SET_DISABLED, SET_PROGRESS } from 'redux/main/mainSlice'
 
 
 
 // utils
-import { allDBusersquery, userSettingsQueryString } from 'graphql/queries';
+import { allDBdataquery, allDBusersquery, userSettingsQueryString, addUserSettingsQueryStringFunc, getUserDailyDataQueryString } from 'graphql/queries';
 import {MainInterface, SettingsInterface} from "utility/interfaceNtypes"
 import {usePromise} from "Contexts/Promises"
 import waterIntakeWeightFormula from "utility/waterIntakeWeightFormula"
 
 
-export default function Main() {
+export default function Main(props) {
+
   
-    
   return (
     <Container id={styles.Page_1}>
       {/*  functional component below has HYDRO_SETTINGS props which are then put into the props of component during renderMain() */}
-      <RenderMain/>
+  <RenderMain setLocalCurrentUsername={props.setLocalCurrentUsername} mainBorderHover={props.mainBorderHover} setMainBorderHover={props.setMainBorderHover} />
     </Container>
   );
 }
 
-function RenderMain() {  
-  // const { hydroSchedule, hydroIntake, status, setStatus, disabled, setDisabled, HYDRO_SCHEDULE } = props
+function RenderMain(props: any) {    
+
+  const dispatch = useDispatch()
 
   const HYDRO_SETTINGS = useSelector( (state:RootState) => state.main.HYDRO_SETTINGS)
   const HYDRO_SCHEDULE = useSelector( (state:RootState) => state.main.HYDRO_SCHEDULE)
   const HYDRO_INTAKE = useSelector( (state:RootState) => state.main.HYDRO_INTAKE)
   const STATUS = useSelector( (state:RootState) => state.main.STATUS)
   const DISABLED = useSelector( (state:RootState) => state.main.DISABLED)
+  const DATE = useSelector( (state:RootState) => state.main.DATE)
 
   // HYDRO_SETTINGS: false, HYDRO_SCHEDULE: [], HYDRO_INTAKE: 0, STATUS: [], DISABLED: [],
 
   // const HYDRO_SETTINGS = props.HYDRO_SETTINGS
-  const { iPROMISEcookies, getUserSettingsPROMISE, userSettingsSchedulePROMISE, getDailyDataPROMISE, userSettingsIntakePROMISE } = usePromise()
+const { iPROMISEcookies, getAndSetCurrentUserPROMISE, getUserSettingsPROMISE, userSettingsSchedulePROMISE, getDailyDataPROMISE, userSettingsIntakePROMISE, setDataStatePROMISE, setUserSettingsPROMISE } = usePromise()
 
 
   useEffect(() => { 
-    dispatch(SET_CURRENT_PAGE("/"))
-    iPROMISEcookies()
-    .then(async(cookie) => {
-      const IDcookieINT = parseInt(cookie)
-      const allDBusers = await axios.post('/api/graphql', { query: `${allDBusersquery}` })
-      if (!Number.isNaN(IDcookieINT)) {
-        const allUsers = allDBusers.data.data.allDBusers
-        const meUser = allUsers.find(user => user.id === IDcookieINT)
 
-        let promisebank:any[] = [userSettingsSchedulePROMISE(), getDailyDataPROMISE(), userSettingsIntakePROMISE()]
-        Promise.all(promisebank)
+    getAndSetCurrentUserPROMISE(props.setLocalCurrentUsername)
 
-              
-      }
+    dispatch(SET_CURRENT_PAGE("/"));    
+    // export const addUserSettingsQueryStringFunc = (age:number, height:number, weight:number, start_time:number, end_time:number, reminder:number, activity:number, users_id:number) => {
+    setUserSettingsPROMISE()
+    .then( (userSettings) => {
+        console.log('userSettings', userSettings)
     })
-
   }, []);
 
-// function RenderMain({ HYDRO_SETTINGS }: { HYDRO_SETTINGS: boolean }) {  
+  useEffect(() => { 
+    console.log('set hydro_intake is running again')
+    setDataStatePROMISE()
+    .then( (alldata) => {
+      console.log('alldata', alldata)
+    })
 
-  const dispatch = useDispatch()
+    setUserSettingsPROMISE()
+    .then( (userSettings) => {
+        console.log(userSettings)
+
+    })
+  }, [HYDRO_INTAKE]);
+
+// function RenderMain({ HYDRO_SETTINGS }: { HYDRO_SETTINGS: boolean }) {  
 
   const test = () => {
     console.log('hydro main', HYDRO_SETTINGS)
@@ -76,24 +86,46 @@ function RenderMain() {
 
   return (
     <>
-      <Container onClick={test} className={styles.primary}>
+      <Container onMouseEnter={() => props.setMainBorderHover(true)} onMouseLeave={() => props.setMainBorderHover(false)} onClick={test} className={styles.primary}>
+
+          {
+            HYDRO_SCHEDULE && STATUS
+                  ?
+              <>
+              <Schedule/>
+              </>
+                  :
+            <Dropinbucket />
+          }
+
+          {/* <Dropinbucket /> */}
+
         <div className={styles.display}>
-          {/* { HYDRO_SETTINGS === true && <Dropinbucket /> } */}
-          <Dropinbucket />
-          {/* <p style={{ color: 'black'}}> display </p> */}
+
         </div>
 
-        <Container className={styles.schedule}></Container>
       </Container>
 
       <Container className={styles.panel}>
-        {/* Use the hydroSettings state here */}
       
-        { HYDRO_SETTINGS === true && <Settings/> }
-        
-        {/* { HYDRO_SETTINGS === true && <h1> hey </h1>} */}
-
+        {
+         HYDRO_SETTINGS === true 
+              ?
+         <Settings/> 
+              :
+           <Panel
+           date={DATE}
+           hydroIntake={HYDRO_INTAKE}
+           hydroSchedule={HYDRO_SCHEDULE}
+           mainBorderHover={props.mainBorderHover}
+           />
+          //  <Panel/>
+        }  
+              
+        {/* { HYDRO_SETTINGS === true && <Settings/> }         */}
       </Container>
+      {/* <Credits/> */}
+      
     </>
   );
 }
