eff47356a0cd2860c7f50cb24231b37d5326ac9b
Frank Collins <fwc3rd@gmail.com>
2024-07-03 15:54:20 -0400

    delete and repeat [3:54pm]

--- redis/redis.js
+++
@@ -1,4 +0,0 @@
-// redis.js createClient. This is so a /pages.file.tsx ----> multiple components can import the redisClient and use the cached
-import redis from 'redis';
-const redisClient = redis.createClient();
-export default redisClient;
