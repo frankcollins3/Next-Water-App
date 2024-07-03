f02203b38171074e5780bc265554938014ca4fdf
Frank Collins <fwc3rd@gmail.com>
2024-07-03 17:28:00 -0400

    dupe | dep [5:27pm]

--- components/elements/WaterChart/WaterChart.tsx
+++ components/elements/WaterChart/WaterChart.tsx
@@ -1,2 +1,9 @@
-import { LineChart, Line } from 'recharts';
-const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
+export default function WaterChart() {
+    return (
+        <p> hey </p> 
+    )
+}
+
+
+// import { LineChart, Line } from 'recharts';
+// const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}, ...];
